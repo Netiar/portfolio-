cef3e01e98258ea20e8bcb32ee6e4ec35e8b4a8b
Raindrop <collins.odinga@student.moringaschool.com>
2022-02-28 01:10:20 +0300

    feature:adding functionality to clear messages

--- js/index.js
+++ js/index.js
@@ -1,20 +1,91 @@
-//get data
-const nameInput = document.querySelector("#name")
-const email= document.querySelector("#email")
-const message = document.querySelector("#message")
-const success = document.querySelector("#success")
-const errorNodes = document.querySelectorAll(".error")
+//Get data
+const nameInput = document.querySelector("#name");
+const email= document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
 
 
 
-//validateform
-function validateform() 
-{
+//validate data
+function validateForm(){
      clearMessages();
-      
-     if(nameInput.value.length < 1)
-     {
-         errorNodes[0].innerText = "Please enter a name"
+     let errorFlag =false;
+
+     if(nameInput.value.length < 1){
+         errorNodes[0].innerText = "blank";
          nameInput.classList.add(error-border);
+         errorFlag = true;
      }
+
+     if(!emailIsValid(email.value)){
+        errorNodes[1].innerText = "invalid email address";
+        email.classList.add(error-border);
+        errorFlag = true;
+     }
+
+     if(message.value.length < 1){
+        errorNodes[2].innerText = "Please enter message"
+        message.classList.add(error-border);
+        errorFlag = true;   
+
+    }
+
+     if(!errorFlag){
+         success.innerText = "success!"
+     }
+
+
+
 }
+
+//clear error/ success message
+function clearMessages(){
+     for(let i = 0; i < errorNodes.length; i++) 
+     {
+         errorNodes[i].innerText = "";
+     }
+     success.innerText = "";
+     nameInput.classlist.remove("error-border");
+     email.classlist.remove("error-border");
+     message.classlist.remove("error-border");
+}
+
+
+//check if email is valid
+function emailIsValid(email){
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
